3ae121944f139fb686d6befb6d3cf6c794237265
vinukab <vinukabuddhima32@gmail.com>
2025-04-20 07:55:45 +0530

    created a dummy profile route

---
+++ src/app/profile/page.tsx
@@ -0,0 +1,13 @@
+// app/profile/page.tsx
+
+export default function ProfilePage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold mb-4">Your Profile</h1>
+      <p className="text-gray-700">
+        Welcome to your profile page. Here you can view and edit your
+        information.
+      </p>
+    </div>
+  );
+}
