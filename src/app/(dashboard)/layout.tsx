f775f4c2176b79a8c2defc79e67628c3d2801bb6
Dasun Piyumal <w.piyumal2319@gmail.com>
2025-05-19 23:03:37 +0530

    feat: add HeaderSearchBar component for enhanced stock search functionality and loading animation

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,12 +1,12 @@
 import {AppSidebar} from "@/app/(dashboard)/_components/app-sidebar"
 import {Separator} from "@/components/ui/separator"
 import {SidebarInset, SidebarProvider, SidebarTrigger,} from "@/components/ui/sidebar"
-import {Bell, LogOut} from "lucide-react"
+import {LogOut} from "lucide-react"
 import {ModeToggle} from "@/components/ThemeProvider";
-import {Button} from "@/components/ui/button";
 import React, {Suspense} from "react";
 import {SidebarSkeleton} from "@/app/(dashboard)/_components/sidebar-skeleton";
 import Link from "next/link";
+import HeaderSearchBar from "./_components/HeaderSearchBar"; // Import the new component
 
 export default function DashboardLayout({children}: { children: React.ReactNode }) {
     return (
@@ -20,20 +20,20 @@ export default function DashboardLayout({children}: { children: React.ReactNode 
                     <div className="flex items-center gap-2 px-4">
                         <SidebarTrigger className="-ml-1"/>
                     </div>
+
+                    {/* Search bar in the middle */}
+                    <div className="flex-1 max-w-md px-4">
+                        <HeaderSearchBar className="w-full"/>
+                    </div>
+
                     <div className="flex items-center gap-4 px-4">
                         <ModeToggle/>
                         <Separator
                             orientation="vertical"
                             className="data-[orientation=vertical]:h-4"/>
-                        <Button variant={"ghost"} size={"icon"}>
-                            <Bell size={16}/>
-                        </Button>
-                        <Separator
-                            orientation="vertical"
-                            className="data-[orientation=vertical]:h-4"
-                        />
-                        <Link href="/logout" passHref >
-                            <button className={'cursor-pointer hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50 w-8 h-8 rounded-sm flex items-center justify-center transition-colors'}>
+                        <Link href="/logout" passHref>
+                            <button
+                                className={'cursor-pointer hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50 w-8 h-8 rounded-sm flex items-center justify-center transition-colors'}>
                                 <LogOut size={16}/>
                             </button>
                         </Link>
